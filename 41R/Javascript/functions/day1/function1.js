5671d7fbe56ece0a193f7c24b56bd84539067f02
uday kumar <uday33379@gmail.com>
2025-07-16 17:16:39 +0530

    Added js functions task 1

---
+++ 41R/Javascript/functions/day1/function1.js
@@ -0,0 +1,51 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+
+// 1
+function greet(){
+    console.log("hello!")
+}
+greet();
+
+//2 
+
+function showMessage(){
+    console.log("This is a message!")
+}
+showMessage();
+
+
+// 3
+function LogInInfo(){
+    console.log("Logging Information")
+}
+LogInInfo()
+
+// 4
+function startGame(){
+    console.log("game started")
+    endGame()
+}
+function endGame(){
+    console.log("Game Over!")
+}
+startGame()
+
+
+// 5
+
+function beginTask(){
+    console.log("Task Started")
+    finishTask()
+}
+function finishTask(){
+    console.log("task Finished")
+}
+beginTask();
+
+
+
+
+
